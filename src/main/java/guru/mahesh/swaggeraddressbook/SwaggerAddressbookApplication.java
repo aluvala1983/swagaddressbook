7fb37fd2f9bcfae2cc159a1b7ab2cb8c0239cc8e
aluvala1983 <mahesh.aluvala@gmail.com>
2019-10-10 19:32:52 +0530

    first Commit without endpoint and swagger

---
+++ src/main/java/guru/mahesh/swaggeraddressbook/SwaggerAddressbookApplication.java
@@ -0,0 +1,13 @@
+package guru.mahesh.swaggeraddressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerAddressbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerAddressbookApplication.class, args);
+	}
+
+}
