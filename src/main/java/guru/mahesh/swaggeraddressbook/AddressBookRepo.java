a67477e0f70272cd20dd0d6b2a606dca649aa932
aluvala1983 <mahesh.aluvala@gmail.com>
2019-10-11 21:50:02 +0530

    After adding Swagger

--- src/main/java/guru/mahesh/swaggeraddressbook/AddressBookRepo.java
+++ src/main/java/guru/mahesh/swaggeraddressbook/AddressBookRepo.java
@@ -9,14 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@RequestMapping("/api")
 public class AddressBookRepo {
 	
 	ConcurrentMap<String,Contact> contacts=new ConcurrentHashMap<>();
 	
 	@GetMapping("/{conId}")
+	@ApiOperation(value="Search contact by ConId",notes="provide an id and search the details",response=Contact.class)
 	public Contact getContact(@PathVariable String conId)
 	{
 		return contacts.get(conId);
--- src/main/java/guru/mahesh/swaggeraddressbook/SwaggerAddressbookApplication.java
+++ src/main/java/guru/mahesh/swaggeraddressbook/SwaggerAddressbookApplication.java
@@ -1,13 +1,43 @@
 package guru.mahesh.swaggeraddressbook;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class SwaggerAddressbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerAddressbookApplication.class, args);
 	}
+	
 
+	@Bean
+	public Docket swaggerconfigueration()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.paths(PathSelectors.ant("/api/*"))
+				.apis(RequestHandlerSelectors.basePackage("guru.mahesh"))
+				.build()
+				.apiInfo(apidetails());
+	}
+
+	private ApiInfo apidetails()
+	{
+		return new ApiInfo("Address Store API","Mahesh Tryout API","1.0.0","Open use",new springfox.documentation.service.Contact("Mahesh","helloworld.in", "man@man.in"),"API","helloworld.in",Collections.emptyList());
+	}
+	
 }
